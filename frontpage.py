d30d8efd0ab9106b1340d8a4351723de796a6bab
raartsunica <raarts@unica.nl>
2025-02-04 21:14:48 +0100

    Update frontpage.py

--- frontpage.py
+++ frontpage.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import Calc_2_WBS
 import activity_resource  # Import but don’t execute immediately
+import d365fo_connect # Import but don't execute immediately
 
-st.title("Project Resource Validation App")
+st.title("Calculatie converter App")
 
 # Selection for navigation
-page = st.selectbox("Select a function:", ["Home", "Calc_2_WBS", "Activity Resource Validation"])
+page = st.selectbox("Select a function:", ["Home", "Calculatie naar WBS", "Activity Resource Validation", "D365FO connector"])
 
 if page == "Calc_2_WBS":
     Calc_2_WBS.main()  # Ensure `Calc_2_WBS.py` has a `main()` function
 
 elif page == "Activity Resource Validation":
     activity_resource.main()  # Ensure `activity_resource.py` has a `main()` function
+
+elif page == "D365FO Connector":
+    d365fo_connect.main()  # Ensure `activity_resource.py` has a `main()` function
