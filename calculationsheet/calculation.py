dffba21b9702dd441df1dc85e75581c536a7b15d
raartsunica <raarts@unica.nl>
2025-02-06 08:56:52 +0100

    Update calculation.py

--- calculationsheet/calculation.py
+++ calculationsheet/calculation.py
@@ -14,9 +14,6 @@ def generate_wbs(df, hierarchy_cols, group_cols, sum_cols):
     
     # Groeperen en sommeren, en hiërarchie-kolommen behouden
     agg_dict = {col: 'sum' for col in sum_cols}
-    for col in hierarchy_cols:
-        agg_dict[col] = 'first'
-    
     grouped_df = df.groupby(group_cols, as_index=False).agg(agg_dict)
     
     # Genereer "Omschrijving" kolom met underscore gescheiden waarden uit de hiërarchie kolommen
@@ -24,12 +21,12 @@ def generate_wbs(df, hierarchy_cols, group_cols, sum_cols):
     
     # Hiërarchie genereren inclusief aggregatieniveaus
     grouped_df = grouped_df.sort_values(by=group_cols)
-    hierarchy_numbers = []
     expanded_rows = []
     counters = {}
     
     for _, row in grouped_df.iterrows():
         level_keys = [row[col] for col in group_cols]
+        wbs_number = []
         
         for i in range(len(level_keys)):
             key = tuple(level_keys[:i+1])
@@ -38,28 +35,26 @@ def generate_wbs(df, hierarchy_cols, group_cols, sum_cols):
             else:
                 counters[key] += 1
             
-            # Reset lagere niveaus
-            lower_keys = list(counters.keys())
-            for lk in lower_keys:
-                if len(lk) > i+1 and lk[:i+1] == key:
-                    counters[lk] = 1
+            # Reset lagere niveaus als een hoger niveau verandert
+            for sub_key in list(counters.keys()):
+                if len(sub_key) > i+1 and sub_key[:i+1] == key:
+                    counters[sub_key] = 1
+            
+            wbs_number.append(str(counters[key]))
+            
+            # Voeg aggregatieniveau toe als het nog niet bestaat
+            if i < len(level_keys) - 1:
+                parent_key = level_keys[:i+1] + ["00"] * (len(level_keys) - (i+1))
+                if tuple(parent_key) not in counters:
+                    parent_row = {col: parent_key[j] for j, col in enumerate(group_cols)}
+                    parent_row.update({col: "00" for col in hierarchy_cols if col not in group_cols})
+                    parent_row.update({col: 0 for col in sum_cols})
+                    parent_row["Omschrijving"] = "_".join(parent_key)
+                    parent_row["WBS"] = ".".join(wbs_number)
+                    expanded_rows.append(parent_row)
         
-        # Genereer hiërarchisch nummer
-        number = ".".join(str(counters[tuple(level_keys[:i+1])]) for i in range(len(level_keys)))
         row_dict = row.to_dict()
-        row_dict["WBS"] = number
-        hierarchy_numbers.append(number)
-        
-        # Voeg aggregatieniveaus toe
-        for i in range(len(level_keys)):
-            parent_key = level_keys[:i+1] + ["00"] * (len(level_keys) - (i+1))
-            parent_row = {col: parent_key[j] for j, col in enumerate(group_cols)}
-            parent_row.update({col: "00" for col in hierarchy_cols if col not in group_cols})
-            parent_row.update({col: 0 for col in sum_cols})
-            parent_row["Omschrijving"] = "_".join(parent_key)
-            parent_row["WBS"] = ".".join(str(counters[tuple(parent_key[:j+1])]) for j in range(i+1))
-            expanded_rows.append(parent_row)
-        
+        row_dict["WBS"] = ".".join(wbs_number)
         expanded_rows.append(row_dict)
     
     expanded_df = pd.DataFrame(expanded_rows)
